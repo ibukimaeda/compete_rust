d9aa04b8b00ee82ff72b44f83afd0087cf22002e
maeda <maeda.ibuki554@mail.kyutech.jp>
2024-06-26 11:59:25 +0900

    virtual contest も利用できるように

--- new.sh
+++ new.sh
@@ -1,8 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-cargo compete new $1
+# 引数の数をチェック
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 {c|vc} <project_name>"
+  exit 1
+fi
 
-# echo "$1/*" >> .gitignore
-cargo member include $1
-
-cd $1
+# 引数の処理
+case $1 in
+  c)
+    cargo compete new $2
+    cargo member include $2
+    cd $2
+    ;;
+  vc)
+    cd virtual_contest
+    cargo compete new $2
+    cd ..
+    cargo member include virtual_contest/$2
+    cd virtual_contest/$2
+    ;;
+  *)
+    echo "Invalid option: $1"
+    echo "Usage: $0 {c|vc} <project_name>"
+    exit 1
+    ;;
+esac
