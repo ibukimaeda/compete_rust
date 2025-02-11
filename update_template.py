958a3ab8105bdc91d38973896a3efb5b0208573a
maeda <maeda.ibuki554@mail.kyutech.jp>
2025-02-11 11:43:01 +0900

    update update_template.py

--- update_template.py
+++ update_template.py
@@ -5,14 +5,15 @@ with open("src/template.rs", "r") as src_file:
     src_content = src_file.read()
 
 for dir in ["contest", "virtual_contest"]:
-    toml = TOMLFile(f"./{dir}/compete.toml")
+    toml_file_name = f"./{dir}/compete.toml"
+    toml = TOMLFile(toml_file_name)
     toml_data = toml.read()
     template = toml_data.get("template")
     template["src"] = f"""{src_content}\n"""
 
     toml.write(toml_data)
+    subprocess.run(["git", "add", toml_file_name])
 
-
-subprocess.run(["git", "add", "."])
+subprocess.run(["git", "add", "./src/template.rs"])
 subprocess.run(["git", "commit", "-m", "update template"])
 subprocess.run(["git", "push", "origin", "main"])
